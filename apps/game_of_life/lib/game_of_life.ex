e4457a11bb12935cee91e037860a50968c36d252
Eiji MATSUMOTO <e.mattsan@gmail.com>
2022-06-13 22:04:21 +0900

    remove an implementation with ex codes

--- apps/game_of_life/lib/game_of_life.ex
+++ apps/game_of_life/lib/game_of_life.ex
@@ -39,65 +39,6 @@ defmodule GameOfLife do
     end)
   end
 
-  def run_ex(count \\ 10_000) do
-    Owl.ProgressBar.start(
-      id: :count,
-      label: "count",
-      total: count,
-      timer: true,
-      bar_width_ratio: 0.85
-    )
-
-    1..count
-    |> Enum.reduce(:array.from_list(String.to_charlist(seed())), fn i, field ->
-      pngex =
-        Pngex.new(
-          type: :indexed,
-          width: @width,
-          height: @height,
-          depth: :depth8,
-          palette: @palette
-        )
-
-      image = Pngex.generate(pngex, :array.to_list(field))
-      File.write(filename(i), image)
-
-      Owl.ProgressBar.inc(id: :count)
-
-      next_field =
-        Enum.flat_map(0..(@height - 1), fn y ->
-          Enum.map(0..(@width - 1), fn x ->
-            left = rem(x + @width - 1, @width)
-            right = rem(x + 1, @width)
-            top = rem(y + @height - 1, @height)
-            bottom = rem(y + 1, @height)
-
-            sum =
-              [
-                :array.get(top * @width + left, field),
-                :array.get(top * @width + x, field),
-                :array.get(top * @width + right, field),
-                :array.get(y * @width + left, field),
-                :array.get(y * @width + right, field),
-                :array.get(bottom * @width + left, field),
-                :array.get(bottom * @width + x, field),
-                :array.get(bottom * @width + right, field)
-              ]
-              |> Enum.sum()
-
-            case {:array.get(y * @width + x, field), sum} do
-              {1, 2} -> 1
-              {1, 3} -> 1
-              {0, 3} -> 1
-              _ -> 0
-            end
-          end)
-        end)
-
-      :array.from_list(next_field)
-    end)
-  end
-
   @spec filename(non_neg_integer()) :: String.t()
   defp filename(i) do
     "images/field-#{String.pad_leading(to_string(i), 5, "0")}.png"
